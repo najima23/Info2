7173e5f4d70d9546034e89a966df1e259f5ede28
Najima <najima23.1@gmail.com>
2017-12-30 00:30:08 +0100

    remove
    remove commits

--- main.c
+++ main.c
@@ -9,45 +9,6 @@
 
 int main()
 {
-  /*
-     TDate newDate1;
-     TDate newDate2;
-     TPlayer newPlayer;
-     TPlayer newPlayer2;
-     TTeam newTeam;
-
-     TDate *PNewDate;
-
-     PNewDate = &newDate1;
-
-     newDate1.Year =  2017;
-     newDate1.Month =  1;
-     newDate1.Day =  2;
-
-     newDate2.Year =  1989;
-     newDate2.Month =  9;
-     newDate2.Day =  19;
-
-     newPlayer.Name= "Hicham";
-     newPlayer.Birthday= PNewDate;
-     newPlayer.Number= 11;
-     newPlayer.Goals= 2;
-
-     newPlayer2.Name= "Hicham";
-     newPlayer2.Birthday= PNewDate;
-     newPlayer2.Number= 11;
-     newPlayer2.Goals= 2;
-
-     newTeam.Name = "Hungry Bitches";
-     newTeam.Coach = "Michael";
-     newTeam.Size = 21;
-     newTeam.Player[0] = newPlayer; 
-     newTeam.Player[1] = newPlayer2; 
-
-     printf("Das Datum ist: %i, %i, %i", newDate2.Year, newDate2.Month, newDate2.Day);
-     printf("Der Player ist: %s, %i, %i %i, %i, %i", newPlayer.Name, newPlayer.Number, newPlayer.Goals, newPlayer.Birthday->Year, newPlayer.Birthday->Month, newPlayer.Birthday->Day);
-     printf("Das Team ist: %s, %s, %i %s, %s", newTeam.Name, newTeam.Coach, newTeam.Size, newTeam.Player[0].Name, newTeam.Player[1].Name);
-     */
   int input, checkSave = 0;
   char *menuTitel = "Mannschaften-Verwaltung V0.4";
   char *menuItems[] = {
